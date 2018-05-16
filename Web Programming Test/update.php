4d158003e9d2b5e31a2312b40daff83ca9d684c7
Luthfi Maulana <30421448+lemi101@users.noreply.github.com>
2018-05-16 16:29:00 +0700

    Update update.php

--- Web Programming Test/update.php
+++ Web Programming Test/update.php
@@ -2,8 +2,12 @@
 include 'koneksi.php';
 
 $id = $_GET['id'];
+
 $query = "SELECT * FROM buku WHERE ID = '$id'";
+$query2 = "SELECT ID, Genre FROM genre";
+
 $exec = $mysqli->query($query) or die ('Query gagal');
+$exec2 = $mysqli->query($query2) or die ('Query gagal');
 
 $row = $exec->fetch_assoc();
 ?>
@@ -11,13 +15,37 @@ $row = $exec->fetch_assoc();
 <body>
   <form action="updater.php" method="POST">
     <b>Judul Buku</b>
-    <input type="text" name="judul" value="<?php $row["Judul"]?>" disabled>
+    <br>
+    <input type="text" name="judul" value="<?php echo($row["Judul"]);?>" required>
+    <br><br>
 
     <b>Pengarang</b>
-    <input type="text" name="pengarang" value="<?php $row["Pengarang"]?>" required>
+    <br>
+    <input type="text" name="pengarang" value="<?php echo($row["Pengarang"]);?>" required>
+    <br><br>
     
-    <b>ID Genre</b>
-    <input type="text" name="id_g" value="<?php $row["ID_Genre"]?>" required>
+    <b>Genre</b>
+    <br>
+    <select name="id_g" required>
+      <?php 
+        if ($exec2->num_rows > 0) {
+          while($row2 = $exec2->fetch_assoc()) {
+              $opt= "";
+
+              if ($row2["ID"] == $row["ID_Genre"]) {
+                $opt= "<option value=".$row2["ID"]." selected>".$row2['Genre']."</option>";
+              } else {
+                $opt= "<option value=".$row2["ID"].">".$row2['Genre']."</option>";
+              }
+              
+              echo($opt);
+          }
+      }
+    ?>
+    </select>
+    <br><br>
+
+    <input type="text" name="id_b" value="<?php echo($id);?>" hidden>
 
     <button type="submit">Update!</button>
   </form>
